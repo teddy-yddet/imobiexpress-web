2a1a3a5e38a73eb255f34ed12e667d319d285a7e
sidnei <sidnei.mitani@gmail.com>
2023-05-28 18:01:25 -0300

    nome da imobiliária foi mudado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,7 +18,7 @@ const Header = () => {
           <img src={LogoImg} alt="" />
         </Link>
       </Logo>
-      <h1>Imobiliária IFSP</h1>
+      <h1>Imobiliária Federal</h1>
       <Menu>
         <ul>
           {!userLogged ? (
